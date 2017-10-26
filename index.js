5524500472952a738cd2ff3abb775a5478069583
chrisdavetv <chrisdavetv@users.noreply.github.com>
2017-10-26 21:31:51 +0800

    added POST method for chatbot interaciton

--- index.js
+++ index.js
@@ -29,6 +29,19 @@ app.get('/webhook', function (req, res) {
 	res.send('Error, wrong token')
 })
 
+app.post('/webhook', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+	    let event = req.body.entry[0].messaging[i]
+	    let sender = event.sender.id
+	    if (event.message && event.message.text) {
+		    let text = event.message.text
+		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+	    }
+    }
+    res.sendStatus(200)
+})
+
 // Spin up the server
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
