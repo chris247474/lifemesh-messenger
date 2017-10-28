aa52e72258c68e777290d1cc2a128dd9955d999d
chrisdavetv <chrisdavetv@users.noreply.github.com>
2017-10-28 16:56:36 +0800

    aa

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function handleMessage(sender_psid, received_message) {
 	if (received_message.text) {    
 		// Create the payload for a basic text message
 		response = {
-			"text": 'You sent the message: "${received_message.text}"'
+			"text": "You sent the message: " + received_message.text
 		}
 	}else if (received_message.attachments) {
 		// Gets the URL of the message attachment
