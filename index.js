8c23794fa0df1676380fdbcd194596332cc03a92
chrisdavetv <chrisdavetv@users.noreply.github.com>
2017-10-28 16:52:26 +0800

    filling in basic structure

--- index.js
+++ index.js
@@ -33,11 +33,13 @@ app.post('/webhook', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
 	    let event = req.body.entry[0].messaging[i]
-	    let sender = event.sender.id
+		let sender = event.sender.id
+		
 	    if (event.message && event.message.text) {
-		    let text = event.message.text
-		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-	    }
+			handleMessage(sender, event.message)
+	    }else if (event.postback){
+			handlePostback(sender, event.postback)
+		}
     }
     res.sendStatus(200)
 })
@@ -47,21 +49,81 @@ app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 })
 
-function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-	    url: 'https://graph.facebook.com/v2.6/me/messages',
-	    qs: {access_token:FBPAGETOKEN},
-	    method: 'POST',
-		json: {
-		    recipient: {id:sender},
-			message: messageData,
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+	//let text = event.message.text
+	//sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+
+	let response;
+
+	// Check if the message contains text
+	if (received_message.text) {    
+		// Create the payload for a basic text message
+		response = {
+			"text": 'You sent the message: "${received_message.text}"'
 		}
-	}, function(error, response, body) {
-		if (error) {
-		    console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-		    console.log('Error: ', response.body.error)
-	    }
-    })
+	}else if (received_message.attachments) {
+		// Gets the URL of the message attachment
+		let attachment_url = received_message.attachments[0].payload.url;
+		// do something w it
+	} 
+	
+	// Sends the response message
+	callSendAPI(sender_psid, response); 
 }
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+	sendTextMessage(sender, "Received postback event: " + received_postback)
+
+	let response;
+	
+	// Get the payload for the postback
+	let payload = received_postback.payload;
+
+	// do something with payload
+}
+
+function sendTextMessage(sender, text) {
+	let messageData = { text:text }
+	callSendAPI(sender, messageData)
+}
+
+function callSendAPI(sender_psid, response) {
+	// Construct the message body
+	let request_body = {
+	  "recipient": {
+		"id": sender_psid
+	  },
+	  "message": response
+	}
+
+	sendPostReq(request_body)
+  }
+
+function setGetStartedPostback(getStartedPayloadText){
+	// Construct the message body
+	let request_body = {
+		"getstarted": getStartedPayloadText
+	}
+
+	sendPostReq(request_body)
+}
+
+function sendPostReq(request_body){
+	// Send the HTTP request to the Messenger Platform
+	request({
+		"uri": "https://graph.facebook.com/v2.6/me/messages",
+		"qs": { "access_token": FBPAGETOKEN },
+		"method": "POST",
+		"json": request_body
+	}, (err, res, body) => {
+		if (!err) {
+			console.log('request/message sent by ' + sender_psid)
+		} else {
+			console.error("Unable to send message:" + err);
+		}
+	}); 
+}
+
+//helper layer
