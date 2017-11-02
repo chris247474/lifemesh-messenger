1185c12874e12b247d2de7c5f7b1074e5ecdba02
chrisdavetv <chrisdavetv@users.noreply.github.com>
2017-11-02 12:21:19 +0800

    saving progress, working on web app first as decided by team

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const request = require('request')
 const app = express()
 
 const TOKEN = 'lm4thewin'
-const FBPAGETOKEN = "EAAB1CdftuT0BAPF5iwrwAFHWSa7pHamq1L45wBAIlWXDqDY20NTaEm9f645mZCQbw0IlnZAQpKXtjVIt7vuiQAz6EUhT2xFtKfx9mOVHR7IyeiTPjFr7RvcoMD1TcnG1fdCiPhr2nsGd4dm7UvXYEAv09JzvaFiRCL2LbQ7gZDZD"
+const FBPAGETOKEN = "EAAKkgOg8ec4BAAt4h7p0lynlwlDxVlSVH6z6SgdgbhzRZBWABjVY2VgerB91qEZAAZBoXSZA1tjcDW1AfUZCLrBkLEdSlZBjSFJsSZBPsHzzThECBCInZBcj4y74Ur4tPeNO6Q8HE27gVLqZARsk8nqR6SJSjbHue2xIKgZA2rZCOHCsQZDZD"
+//"EAAB1CdftuT0BAPF5iwrwAFHWSa7pHamq1L45wBAIlWXDqDY20NTaEm9f645mZCQbw0IlnZAQpKXtjVIt7vuiQAz6EUhT2xFtKfx9mOVHR7IyeiTPjFr7RvcoMD1TcnG1fdCiPhr2nsGd4dm7UvXYEAv09JzvaFiRCL2LbQ7gZDZD"
+
+const GETSTARTEDPOSTBACK = "getstarted"
+const QUICKREPLYSUFFIX = "quickreply"
+const INEEDHELP = "I need help"
+const IWANTTOHELP = "I want to help"
+
+const minConfidence = 0.8
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -30,19 +38,28 @@ app.get('/webhook', function (req, res) {
 })
 
 app.post('/webhook', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-	    let event = req.body.entry[0].messaging[i]
-		let sender = event.sender.id
-		
-	    if (event.message && event.message.text) {
-			handleMessage(sender, event.message)
-	    }else if (event.postback){
-			handlePostback(sender, event.postback)
+	let messaging_events = req.body.entry[0].messaging
+	console.log("received webhook req: " + req + ", body: "+req.body + ", entry[0]:" + req.body.entry[0] + ", messaging: "+req.body.entry[0].messaging)
+    try{
+		for (let i = 0; i < messaging_events.length; i++) {
+			let event = req.body.entry[0].messaging[i]
+			let sender = event.sender.id
+			
+			if (event.message && event.message.text) {
+				handleMessage(sender, event.message)
+			}else if (event.postback){
+				handlePostback(sender, event.postback)
+			}
 		}
-    }
+	}catch(e){
+		console.log("webhook error: " + e)
+	}
     res.sendStatus(200)
 })
+
+// do some initial bot setup
+setGetStartedPostback();
+setPersistenceMenu();
 
 // Spin up the server
 app.listen(app.get('port'), function() {
@@ -51,42 +68,97 @@ app.listen(app.get('port'), function() {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
-	//let text = event.message.text
-	//sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-
-	let response;
-
-	// Check if the message contains text
-	if (received_message.text) {    
-		// Create the payload for a basic text message
-		response = {
-			"text": "You sent the message: " + received_message.text
-		}
-	}else if (received_message.attachments) {
-		// Gets the URL of the message attachment
-		let attachment_url = received_message.attachments[0].payload.url;
-		// do something w it
-	} 
+	let message = received_message.text
+	console.log("in handleMessage: "+message)
+	var helpEntity = firstEntity(received_message.nlp, "helpIntent")
+	var wantEntity = firstEntity(received_message.nlp, "wantIntent")
+	var needDescEntity = firstEntity(received_message.nlp, "needDescription")
+	var text = ""
 	
-	// Sends the response message
-	callSendAPI(sender_psid, response); 
+	if(helpEntity && helpEntity.confidence > minConfidence){
+		text = "What does your community need? Were you just hit by a disaster? Take some time to describe your situation and your needs in a single message"
+		sendTextMessage(sender_psid, text)
+	}else if(wantEntity && wantEntity.confidence > minConfidence){
+		text = "What can you provide? Take some time to describe your organization's profile and what type of relief you can offer in a single message"
+		sendTextMessage(sender_psid, text)
+	}else if(needDescEntity && needDescEntity.confidence > minConfidence){
+		text = "Sending your need out to NGOs around the world!\n"
+		try{
+			var quantities, types = extractNeedQuantity(needDescEntity)
+			for (var c = 0;c < quantities.length && c < types.length && types.length == quantities.length;c++){
+				text = text + "Need: " + quantities[c] + " " + types[c] + ", "
+			}
+			sendQuickReply(sender_psid, text, [
+				createQuickReplyOption("Send"),
+				createQuickReplyOption("Edit")
+			])
+		}catch(e){
+			console.log("extractNeedQuantity error: " + e)
+			sendTextMessage(sender_psid, "Sorry, I didn't quite understand that. Please try again")
+		}
+	}
 }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
-	sendTextMessage(sender, "Received postback event: " + received_postback)
+	console.log("Received postback w payload:" + received_postback.payload+" from: " + sender_psid)
 
 	let response;
 	
 	// Get the payload for the postback
 	let payload = received_postback.payload;
 
-	// do something with payload
+	if(payload == GETSTARTEDPOSTBACK){
+		let welcomeMessage = "Welcome to LifeMesh!"
+		sendQuickReply(sender_psid, welcomeMessage, [
+			createQuickReplyOption(INEEDHELP),
+			createQuickReplyOption(IWANTTOHELP)
+		])
+	}
 }
 
-function sendTextMessage(sender, text) {
+function extractNeedQuantity(needDescriptionEntity){
+	var quantities = []
+	var types = []
+
+	if(needDescriptionEntity && needDescriptionEntity.confidence > minConfidence){
+		for (var quantity in needDescriptionEntity.entities['needQuantity']){
+			quantities.push(quantity.value)
+			console.log("found new quantity: " + quantity.value)
+		}
+		for (var type in needDescriptionEntity.entities['needType']){
+			types.push(type.value)
+			console.log("found new type: " + type.value)
+		}
+	}
+	
+	return (quantities, types)
+}
+
+function firstEntity(nlp, name) {
+	return nlp && nlp.entities && nlp.entities && nlp.entities[name] && nlp.entities[name][0];
+}
+
+function sendTextMessage(sender_psid, text) {
 	let messageData = { text:text }
-	callSendAPI(sender, messageData)
+	callSendAPI(sender_psid, messageData)
+}
+function sendQuickReply(sender_psid, text, quickreplies){
+	let response = {
+		"text": text,
+		"quick_replies": quickreplies
+	}
+	callSendAPI(sender_psid, response)
+}
+function createQuickReplyOption(buttonTxt){
+	return {
+		"content_type":"text",
+		"title":buttonTxt,
+		"payload":constructQuickReplyOptionPayload(buttonTxt)
+	}
+}
+function constructQuickReplyOptionPayload(quickreplytext){
+	return quickreplytext + QUICKREPLYSUFFIX
 }
 
 function callSendAPI(sender_psid, response) {
@@ -101,13 +173,40 @@ function callSendAPI(sender_psid, response) {
 	sendPostReq(request_body, sender_psid)
   }
 
-function setGetStartedPostback(getStartedPayloadText){
+function setPersistenceMenu(){}
+
+function setGetStartedPostback(){
 	// Construct the message body
 	let request_body = {
-		"getstarted": getStartedPayloadText
+		"getstarted": GETSTARTEDPOSTBACK
 	}
 
-	sendPostReq(request_body)
+	// Send the HTTP request to the Messenger Platform
+	// Set GetStarted postback payload
+	request({
+		/*"uri": "https://graph.facebook.com/v2.6/me/messenger_profile",
+		"qs": { "access_token": FBPAGETOKEN },
+		"method": "POST",
+		"json": request_body*/
+		method: 'POST',
+		uri: 'https://graph.facebook.com/v2.6/me/thread_settings',
+		qs: {
+		  access_token: FBPAGETOKEN
+		},
+		json: {
+		  setting_type: 'call_to_actions',
+		  thread_state: 'new_thread',
+		  call_to_actions: [{
+					"payload": GETSTARTEDPOSTBACK
+			}]
+		}
+	}, (err, res, body) => {
+		if (!err) {
+			console.log('GetStarted sent')
+		} else {
+			console.error("Unable to send message:" + err);
+		}
+	}); 
 }
 
 function sendPostReq(request_body, sender_psid){
